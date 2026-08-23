09a0918beb7af304a6d2d6fa8dabb606764fbc94
HardikVachheta <vachhetahardik987@gmail.com>
2026-08-23 21:19:32 +0530

    change version of vite

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '[https://dhdymlzzwxxbuasqdako.supabase.co](https://dhdymlzzwxxbuasqdako.supabase.co)';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-=======
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
@@ -13,4 +5,3 @@ const supabaseUrl =
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
->>>>>>> 1be58d519666f37e8c350e69423fd2a402901530
